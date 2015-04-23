eb93fe13c3e074acdd7826bcadf2fc04dca3190e
KMK73 <kmk73@miami.edu>
2015-04-22 22:43:23 -0400

    app works with workouts date and name showing in table

--- Peak 360 Crossfit App Swift/Peak 360 Crossfit App Swift/WODViewController.swift
+++ Peak 360 Crossfit App Swift/Peak 360 Crossfit App Swift/WODViewController.swift
@@ -1,84 +1,105 @@
 //
-//  FirstViewController.swift
-//  Peak 360 Crossfit App Swift
+//  WODViewController.swift
+//  iTunesPreviewTutorial
 //
-//  Created by Kelsey Kjeldsen on 3/29/15.
-//  Copyright (c) 2015 Kelsey Kjeldsen. All rights reserved.
+//  Created by Jameson Quave on 4/16/15.
+//  Copyright (c) 2015 JQ Software LLC. All rights reserved.
 //
 
 import UIKit
 
-class WODViewController: UIViewController{
-    
-    @IBOutlet weak var WorkoutTitleLabel: UILabel!
-    @IBOutlet weak var WorkoutDescriptionLabel: UILabel!
-    @IBOutlet weak var WodTableView: UITableView!
-
-    var workouts: [WorkoutApi]!
-    var cellHeight: CGFloat = 240
-    
-    var repositories = [String]()
-    
+class WODViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol  {
+    var workouts = [Workout]()
+    var api: APIController!
+    let kCellIdentifier: String = "WorkoutCell"
+    var imageCache = [String:UIImage]()
+    @IBOutlet weak var workoutsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let urlAsString = "http://canescrossfitclub.com/api/workouts.php"
-        let url: NSURL  = NSURL(string: urlAsString)!
-        let urlSession = NSURLSession.sharedSession()
-        
-        let jsonQuery = urlSession.dataTaskWithURL(url, completionHandler: { data, response, error -> Void in
-            if (error != nil) {
-                println(error.localizedDescription)
-            }
-            var err: NSError?
-            
-            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as! NSArray?
-            if (err != nil) {
-                println("JSON Error \(err!.localizedDescription)")
-            }
-            
-            println(jsonResult!)
-            
-            self.workouts = [WorkoutApi]()
-            let api = WorkoutApi(workoutDictionary: NSDictionary);)
-            api.loadWorkoutApi(nil)
-        })
-        jsonQuery.resume()
-        
-        
+        api = APIController(delegate: self)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        api.get("http://canescrossfitclub.com/api/workouts.php")
+        //        api.searchItunesFor("Beatles")
     }
-    
-    func didLoadWorkouts (workouts: WorkoutApi[]({
-        self.workouts = workouts
-        //do not know what to put here instead of collectionView
-        UITableView.reloadData()
-        
-    }
-    
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    
-        let cell = tableView.dequeueReusableCellWithIdentifier("WorkoutCell", forIndexPath: indexPath) as! UITableViewCell
-        
-        let workout = workouts[indexPath.row]
-        cell.workoutTitleLabel!.text = workout.wod_name
-        cell.workoutDescriptionLabel!.text= workout.wod_description
-        
-        
-        return cell
-    }
-    
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workouts.count
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workouts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as! UITableViewCell
+        let workout = self.workouts[indexPath.row]
+        
+        // Get the formatted price string for display in the subtitle
+        cell.detailTextLabel?.text = workout.name
+        // Update the textLabel text to use the title from the Album model
+        cell.textLabel?.text = workout.date
+        
+        // Start by setting the cell's image to a static file
+        // Without this, we will end up without an image view!
+        //        cell.imageView?.image = UIImage(named: "Blank52")
+        //
+        //        let thumbnailURLString = album.thumbnailImageURL
+        //        let thumbnailURL = NSURL(string: thumbnailURLString)!
+        //
+        //        // If this image is already cached, don't re-download
+        //        if let img = imageCache[thumbnailURLString] {
+        //            cell.imageView?.image = img
+        //        }
+        //        else {
+        // The image isn't cached, download the img data
+        // We should perform this in a background thread
+        //            let request: NSURLRequest = NSURLRequest(URL: thumbnailURL)
+        //            let mainQueue = NSOperationQueue.mainQueue()
+        //            NSURLConnection.sendAsynchronousRequest(request, queue: mainQueue, completionHandler: { (response, data, error) -> Void in
+        //                if error == nil {
+        //                    // Convert the downloaded data in to a UIImage object
+        //                    let image = UIImage(data: data)
+        //                    // Store the image in to our cache
+        //                    self.imageCache[thumbnailURLString] = image
+        //                    // Update the cell
+        //                    dispatch_async(dispatch_get_main_queue(), {
+        //                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+        //                            cellToUpdate.imageView?.image = image
+        //                        }
+        //                    })
+        //                }
+        //                else {
+        //                    println("Error: \(error.localizedDescription)")
+        //                }
+        //            })
+        //        }
+        return cell
+    }
+    
+    func didReceiveAPIResults(results: NSArray) {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.workouts = Workout.workoutsWithJSON(results)
+            self.workoutsTableView!.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        })
+    }
+    
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //        if let detailsViewController: DetailsViewController = segue.destinationViewController as? DetailsViewController {
+    //            var workoutIndex = workoutsTableView!.indexPathForSelectedRow()!.row
+    //            var selectedWorkout = self.workouts[workoutIndex]
+    //            detailsViewController.workout = selectedWorkout
+    //        }
+    //    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.layer.transform = CATransform3DMakeScale(0.1,0.1,1)
+        UIView.animateWithDuration(0.25, animations: {
+            cell.layer.transform = CATransform3DMakeScale(1,1,1)
+        })
+    }
     
 }
+
--- Peak 360 Crossfit App Swift/Peak 360 Crossfit App Swift/WorkoutCell.swift
+++ Peak 360 Crossfit App Swift/Peak 360 Crossfit App Swift/WorkoutCell.swift
@@ -1,18 +1,81 @@
 //
-//  WorkoutCell.swift
-//  Peak 360 Crossfit App Swift
+//  Album.swift
+//  iTunesPreviewTutorial
 //
-//  Created by Kelsey Kjeldsen on 4/19/15.
-//  Copyright (c) 2015 Kelsey Kjeldsen. All rights reserved.
+//  Created by Jameson Quave on 4/16/15.
+//  Copyright (c) 2015 JQ Software LLC. All rights reserved.
 //
 
 import Foundation
-import UIKit
 
-
-class WorkoutCell: UITableViewCell {
+struct Workout {
+    let name: String
+    let date: String
+    let workout_description: String
+//    let largeImageURL: String
+//    let itemURL: String
+//    let artistURL: String
+//    let collectionId: Int
     
-    @IBOutlet var workoutTitleLabel: UILabel!
-    @IBOutlet var workoutDescriptionLabel: UILabel!
-
-}
+    init(name: String, date: String, workout_description: String) {
+        self.name = name
+        self.date = date
+        self.workout_description = workout_description
+//        self.largeImageURL = largeImageURL
+//        self.itemURL = itemURL
+//        self.artistURL = artistURL
+//        self.collectionId = collectionId
+    }
+    
+    static func workoutsWithJSON(results: NSArray) -> [Workout] {
+        // Create an empty array of Workouts to append to from this list
+        var workouts = [Workout]()
+        
+        // Store the results in our table data array
+        if results.count>0 {
+            
+            // Sometimes iTunes returns a collection, not a track, so we check both for the 'name'
+            for result in results {
+                
+                var name = result["name"] as? String
+                var date = result["Date"] as? String
+                var workout_description = result["Description"] as? String
+//                if name == nil {
+//                    name = result["collectionName"] as? String
+//                }
+                
+                // Sometimes price comes in as formattedPrice, sometimes as collectionPrice.. and sometimes it's a float instead of a string. Hooray!
+//                var price = result["formattedPrice"] as? String
+//                if price == nil {
+//                    price = result["collectionPrice"] as? String
+//                    if price == nil {
+//                        var priceFloat: Float? = result["collectionPrice"] as? Float
+//                        var nf: NSNumberFormatter = NSNumberFormatter()
+//                        nf.maximumFractionDigits = 2
+//                        if priceFloat != nil {
+//                            price = "$\(nf.stringFromNumber(priceFloat!)!)"
+//                        }
+//                    }
+//                }
+                
+//                let thumbnailURL = result["artworkUrl60"] as? String ?? ""
+//                let imageURL = result["artworkUrl100"] as? String ?? ""
+//                let artistURL = result["artistViewUrl"] as? String ?? ""
+                
+//                var itemURL = result["collectionViewUrl"] as? String
+//                if itemURL == nil {
+//                    itemURL = result["trackViewUrl"] as? String
+//                }
+                
+//                if let collectionId = result["collectionId"] as? Int {
+                    var newWorkout = Workout(name: name!,
+                        date: date!,
+                        workout_description: workout_description!)
+                    workouts.append(newWorkout)
+                }
+            }
+        return workouts
+        
+        }
+    }
+    
